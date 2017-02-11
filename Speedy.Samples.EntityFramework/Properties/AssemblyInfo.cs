3c900c46e53774750a21cdfcb98d5f989fefd488
Bobby Cannon <bobby.cannon@live.com>
2017-02-11 16:37:43 -0500

    Version bump for release.

--- Speedy.Samples.EntityFramework/Properties/AssemblyInfo.cs
+++ Speedy.Samples.EntityFramework/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.4.24.0")]
-[assembly: AssemblyFileVersion("4.4.24.0")]
+[assembly: AssemblyVersion("4.4.25.0")]
+[assembly: AssemblyFileVersion("4.4.25.0")]
