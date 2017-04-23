d8e2d1845c440450bdcea7354b15fcbcb74d8c1c
Bobby Cannon <bobby.cannon@live.com>
2017-04-23 13:51:51 -0400

    Missed a file on last check in

--- Speedy/Extensions.cs
+++ Speedy/Extensions.cs
@@ -573,6 +573,54 @@ namespace Speedy
 		}
 
 		/// <summary>
+		/// Retrieves the default value for a given Type
+		/// </summary>
+		/// <param name="type"> The Type for which to get the default value </param>
+		/// <returns> The default value for <paramref name="type" /> </returns>
+		/// <remarks>
+		/// If a null Type, a reference Type, or a System.Void Type is supplied, this method always returns null.  If a value type
+		/// is supplied which is not publicly visible or which contains generic parameters, this method will fail with an
+		/// exception.
+		/// </remarks>
+		internal static object GetDefault(this Type type)
+		{
+			// If no Type was supplied, if the Type was a reference type, or if the Type was a System.Void, return null
+			if (type == null || !type.IsValueType || type == typeof(void))
+			{
+				return null;
+			}
+
+			// If the supplied Type has generic parameters, its default value cannot be determined
+			if (type.ContainsGenericParameters)
+			{
+				throw new ArgumentException(
+					"{" + MethodBase.GetCurrentMethod() + "} Error:\n\nThe supplied value type <" + type +
+					"> contains generic parameters, so the default value cannot be retrieved");
+			}
+
+			// If the Type is a primitive type, or if it is another publicly-visible value type (i.e. struct/enum), return a 
+			//  default instance of the value type
+			if (type.IsPrimitive || !type.IsNotPublic)
+			{
+				try
+				{
+					return Activator.CreateInstance(type);
+				}
+				catch (Exception e)
+				{
+					throw new ArgumentException(
+						"{" + MethodBase.GetCurrentMethod() + "} Error:\n\nThe Activator.CreateInstance method could not " +
+						"create a default instance of the supplied value type <" + type +
+						"> (Inner Exception message: \"" + e.Message + "\")", e);
+				}
+			}
+
+			// Fail with exception
+			throw new ArgumentException("{" + MethodBase.GetCurrentMethod() + "} Error:\n\nThe supplied value type <" + type +
+				"> is not a publicly-visible type, so the default value cannot be retrieved");
+		}
+
+		/// <summary>
 		/// Open the file with read/write permission with file read share.
 		/// </summary>
 		/// <param name="info"> The information for the file. </param>
