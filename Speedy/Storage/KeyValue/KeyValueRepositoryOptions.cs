de2c4ad7d85b8831b90cdd2b9eb8c1b0da91c85d
Bobby Cannon <bobby.cannon@live.com>
2019-07-13 08:39:20 -0400

    RC3 release
    Restored the KeyValueRepository

---
+++ Speedy/Storage/KeyValue/KeyValueRepositoryOptions.cs
@@ -0,0 +1,49 @@
+#region References
+
+using System;
+
+#endregion
+
+namespace Speedy.Storage.KeyValue
+{
+	/// <summary>
+	/// Options for a key value repository.
+	/// </summary>
+	public class KeyValueRepositoryOptions
+	{
+		#region Constructors
+
+		/// <summary>
+		/// </summary>
+		public KeyValueRepositoryOptions()
+		{
+			Limit = int.MaxValue;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets a flag to ignore virtual members when saving entities.
+		/// </summary>
+		public bool IgnoreVirtualMembers { get; set; }
+
+		/// <summary>
+		/// Gets or sets the caching limit.
+		/// </summary>
+		public int Limit { get; set; }
+
+		/// <summary>
+		/// Gets or sets the flag to determine if this repository is read only.
+		/// </summary>
+		public bool ReadOnly { get; set; }
+
+		/// <summary>
+		/// Gets or sets the maximum time to cache items.
+		/// </summary>
+		public TimeSpan Timeout { get; set; }
+
+		#endregion
+	}
+}
