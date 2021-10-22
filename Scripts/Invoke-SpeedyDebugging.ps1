9a8618270bb2df8eacc217492f62c9d1fb7231ed
Bobby Cannon <bobby.cannon@live.com>
2021-10-22 15:41:38 -0400

    updates

--- Scripts/Invoke-SpeedyDebugging.ps1
+++ Scripts/Invoke-SpeedyDebugging.ps1
@@ -12,8 +12,8 @@ $scriptPath = $PSScriptRoot.Replace("\Scripts", "")
 
 Clear-Host
 
-$versionFull = "8.1.5.0"
-$version = "8.1.5"
+$versionFull = "8.1.6.0"
+$version = "8.1.6"
 $files = Get-ChildItem $ProjectPath *.csproj -Recurse | Select-Object Fullname
 
 $speedyPR = "<PackageReference Include=`"Speedy`" Version=`"$version`" />"
