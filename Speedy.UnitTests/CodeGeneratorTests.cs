7cb997ab8390e73932d7f291644cec560862dc24
Bobby Cannon <bobby.cannon@live.com>
2022-10-19 20:03:47 -0400

    remove tests

--- Speedy.UnitTests/CodeGeneratorTests.cs
+++ Speedy.UnitTests/CodeGeneratorTests.cs
@@ -10,82 +10,38 @@ using Speedy.Sync;
 
 #endregion
 
-namespace Speedy.UnitTests
+namespace Speedy.UnitTests;
+
+[TestClass]
+public class CodeGeneratorTests
 {
-	[TestClass]
-	public class CodeGeneratorTests
+	#region Methods
+
+	[TestMethod]
+	public void CopyOverloadForICloneableToClipboard()
 	{
-		#region Methods
+		var type = typeof(SyncEngineState);
+		var deepCloneUsesShallowClone = true;
+		var builder = new StringBuilder();
+		var properties = type
+			//.GetCachedProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance)
+			.GetCachedProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance)
+			.Where(x => x.CanWrite)
+			.OrderBy(x => x.Name)
+			.ToList();
 
-		public const double ChangingAltitudeFilterOffset = double.MinValue + 1;
+		builder.AppendLine($"/// <inheritdoc />\r\npublic override {type.Name} DeepClone(int levels = -1)\r\n{{");
 
-		[TestMethod]
-		public void name()
+		if (deepCloneUsesShallowClone)
 		{
-			double t1 = 0.0;
-			double t2 = 1.0;
-
-			(t1.Equals(t2)).Dump();
-
-			// 1.0 E -54
-
-			decimal d3 = decimal.MinValue;
-			decimal d4 = decimal.MinValue +1;
-
-			(d3.Equals(d4)).Dump();
-
-
-			
-
-			var test = double.MinValue;
-			test.Equals(double.MinValue + 1).Dump("1");
-			test.Equals(double.MinValue + 2).Dump("2");
-			test.Equals(double.MinValue + 3).Dump("3");
-			test.Equals(double.MinValue).Dump();
-
-			(test == double.MinValue).Dump();
-			(Math.Abs(test - (double.MinValue )) < double.Epsilon).Dump();
+			builder.AppendLine("\treturn ShallowClone();");
 		}
-
-		[TestMethod]
-		public void CopyOverloadForICloneableToClipboard()
+		else
 		{
-			var type = typeof(SyncEngineState);
-			var deepCloneUsesShallowClone = true;
-			var builder = new StringBuilder();
-			var properties = type
-				//.GetCachedProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance)
-				.GetCachedProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance)
-				.Where(x => x.CanWrite)
-				.OrderBy(x => x.Name)
-				.ToList();
-
-			builder.AppendLine($"/// <inheritdoc />\r\npublic override {type.Name} DeepClone(int levels = -1)\r\n{{");
-
-			if (deepCloneUsesShallowClone)
-			{
-				builder.AppendLine("\treturn ShallowClone();");
-			}
-			else
-			{
-				builder.AppendLine("\t// ****** You can use CodeGeneratorTests.GenerateUpdateWith to update this ******\r\n");
-
-				builder.AppendLine($"\r\n\treturn new {type.Name}\r\n\t{{");
-
-				foreach (var p in properties)
-				{
-					builder.AppendLine($"\t\t{p.Name} = {p.Name},");
-				}
-
-				builder.Remove(builder.Length - 3, 1);
-				builder.AppendLine("\t};");
-			}
-
-			builder.AppendLine("}");
-			builder.AppendLine();
-			builder.AppendLine($"/// <inheritdoc />\r\npublic override {type.Name} ShallowClone()\r\n{{");
 			builder.AppendLine("\t// ****** You can use CodeGeneratorTests.GenerateUpdateWith to update this ******\r\n");
-			builder.AppendLine($"\treturn new {type.Name}\r\n\t{{");
+
+			builder.AppendLine($"\r\n\treturn new {type.Name}\r\n\t{{");
+
 			foreach (var p in properties)
 			{
 				builder.AppendLine($"\t\t{p.Name} = {p.Name},");
@@ -93,92 +49,105 @@ namespace Speedy.UnitTests
 
 			builder.Remove(builder.Length - 3, 1);
 			builder.AppendLine("\t};");
-			builder.AppendLine("}");
-
-			builder.ToString().CopyToClipboard();
-			Console.Write(builder.ToString());
 		}
 
-		[TestMethod]
-		public void CopyOverloadForUpdateWithToClipboard()
+		builder.AppendLine("}");
+		builder.AppendLine();
+		builder.AppendLine($"/// <inheritdoc />\r\npublic override {type.Name} ShallowClone()\r\n{{");
+		builder.AppendLine("\t// ****** You can use CodeGeneratorTests.GenerateUpdateWith to update this ******\r\n");
+		builder.AppendLine($"\treturn new {type.Name}\r\n\t{{");
+		foreach (var p in properties)
 		{
-			var type = typeof(SyncEngineState);
-			var builder = new StringBuilder();
-			builder.AppendLine($"/// <inheritdoc />\r\npublic override void UpdateWith(object update, params string[] exclusions)\r\n{{\r\n\tswitch (update)\r\n\t{{\r\n\t\tcase {type.FullName} options:\r\n\t\t{{\r\n\t\t\tUpdateWith(options, exclusions);\r\n\t\t\treturn;\r\n\t\t}}\r\n\t\tdefault:\r\n\t\t{{\r\n\t\t\tbase.UpdateWith(update, exclusions);\r\n\t\t\treturn;\r\n\t\t}}\r\n\t}}\r\n}}");
-			builder.ToString().CopyToClipboard();
-			Console.Write(builder.ToString());
+			builder.AppendLine($"\t\t{p.Name} = {p.Name},");
 		}
 
-		[TestMethod]
-		public void CreateAsserts()
+		builder.Remove(builder.Length - 3, 1);
+		builder.AppendLine("\t};");
+		builder.AppendLine("}");
+
+		builder.ToString().CopyToClipboard();
+		Console.Write(builder.ToString());
+	}
+
+	[TestMethod]
+	public void CopyOverloadForUpdateWithToClipboard()
+	{
+		var type = typeof(SyncEngineState);
+		var builder = new StringBuilder();
+		builder.AppendLine($"/// <inheritdoc />\r\npublic override void UpdateWith(object update, params string[] exclusions)\r\n{{\r\n\tswitch (update)\r\n\t{{\r\n\t\tcase {type.FullName} options:\r\n\t\t{{\r\n\t\t\tUpdateWith(options, exclusions);\r\n\t\t\treturn;\r\n\t\t}}\r\n\t\tdefault:\r\n\t\t{{\r\n\t\t\tbase.UpdateWith(update, exclusions);\r\n\t\t\treturn;\r\n\t\t}}\r\n\t}}\r\n}}");
+		builder.ToString().CopyToClipboard();
+		Console.Write(builder.ToString());
+	}
+
+	[TestMethod]
+	public void CreateAsserts()
+	{
+		var type = typeof(PartialUpdateTests.MyClass);
+		var builder = new StringBuilder();
+		var properties = type
+			//.GetCachedProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance)
+			.GetCachedProperties(BindingFlags.Public | BindingFlags.Instance)
+			.Where(x => x.CanWrite)
+			.OrderBy(x => x.Name)
+			.ToList();
+
+		foreach (var p in properties)
 		{
-			var type = typeof(PartialUpdateTests.MyClass);
-			var builder = new StringBuilder();
-			var properties = type
-				//.GetCachedProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance)
-				.GetCachedProperties(BindingFlags.Public | BindingFlags.Instance)
-				.Where(x => x.CanWrite)
-				.OrderBy(x => x.Name)
-				.ToList();
-
-			foreach (var p in properties)
-			{
-				builder.AppendLine($"Assert.AreEqual(expected.{p.Name}, actual.{p.Name});");
-			}
-
-			builder.ToString().CopyToClipboard();
-			Console.Write(builder.ToString());
+			builder.AppendLine($"Assert.AreEqual(expected.{p.Name}, actual.{p.Name});");
 		}
 
-		[TestMethod]
-		public void GenerateUpdateWith()
-		{
-			var type = typeof(SyncDevice<int>);
-			var builder = new StringBuilder();
-			var properties = type
-				.GetCachedProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance)
-				//.GetCachedProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Instance)
-				.Where(x => x.CanWrite)
-				.OrderBy(x => x.Name)
-				.ToList();
+		builder.ToString().CopyToClipboard();
+		Console.Write(builder.ToString());
+	}
 
-			builder.AppendLine($@"/// <summary>
+	[TestMethod]
+	public void GenerateUpdateWith()
+	{
+		var type = typeof(SyncDevice<int>);
+		var builder = new StringBuilder();
+		var properties = type
+			.GetCachedProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance)
+			//.GetCachedProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Instance)
+			.Where(x => x.CanWrite)
+			.OrderBy(x => x.Name)
+			.ToList();
+
+		builder.AppendLine($@"/// <summary>
 /// Update the {type.Name} with an update.
 /// </summary>
 /// <param name=""update""> The update to be applied. </param>
 /// <param name=""exclusions""> An optional set of properties to exclude. </param>
 public void UpdateWith({type.Name} update, params string[] exclusions)
 {{");
-			builder.AppendLine("\t// If the update is null then there is nothing to do.");
-			builder.AppendLine("\tif (update == null)\r\n\t{\r\n\t\treturn;\r\n\t}\r\n");
-			builder.AppendLine("\t// ****** You can use CodeGeneratorTests.GenerateUpdateWith to update this ******");
-			builder.AppendLine();
-			builder.AppendLine("\tif (exclusions.Length <= 0)");
-			builder.AppendLine("\t{");
+		builder.AppendLine("\t// If the update is null then there is nothing to do.");
+		builder.AppendLine("\tif (update == null)\r\n\t{\r\n\t\treturn;\r\n\t}\r\n");
+		builder.AppendLine("\t// ****** You can use CodeGeneratorTests.GenerateUpdateWith to update this ******");
+		builder.AppendLine();
+		builder.AppendLine("\tif (exclusions.Length <= 0)");
+		builder.AppendLine("\t{");
 
-			foreach (var p in properties)
-			{
-				builder.AppendLine($"\t\t{p.Name} = update.{p.Name};");
-			}
-
-			builder.AppendLine("\t}");
-			builder.AppendLine("\telse");
-			builder.AppendLine("\t{");
-
-			foreach (var p in properties)
-			{
-				builder.AppendLine($"\t\tthis.IfThen(_ => !exclusions.Contains(nameof({p.Name})), x => x.{p.Name} = update.{p.Name});");
-			}
-
-			builder.AppendLine("\t}\r\n");
-			builder.AppendLine("\t//base.UpdateWith(update, exclusions);");
-			builder.AppendLine("}\r\n");
-			builder.AppendLine($"/// <inheritdoc />\r\npublic override void UpdateWith(object update, params string[] exclusions)\r\n{{\r\n\tswitch (update)\r\n\t{{\r\n\t\tcase {type.Name} options:\r\n\t\t{{\r\n\t\t\tUpdateWith(options, exclusions);\r\n\t\t\treturn;\r\n\t\t}}\r\n\t\tdefault:\r\n\t\t{{\r\n\t\t\tbase.UpdateWith(update, exclusions);\r\n\t\t\treturn;\r\n\t\t}}\r\n\t}}\r\n}}");
-
-			builder.ToString().CopyToClipboard();
-			Console.Write(builder.ToString());
+		foreach (var p in properties)
+		{
+			builder.AppendLine($"\t\t{p.Name} = update.{p.Name};");
 		}
 
-		#endregion
+		builder.AppendLine("\t}");
+		builder.AppendLine("\telse");
+		builder.AppendLine("\t{");
+
+		foreach (var p in properties)
+		{
+			builder.AppendLine($"\t\tthis.IfThen(_ => !exclusions.Contains(nameof({p.Name})), x => x.{p.Name} = update.{p.Name});");
+		}
+
+		builder.AppendLine("\t}\r\n");
+		builder.AppendLine("\t//base.UpdateWith(update, exclusions);");
+		builder.AppendLine("}\r\n");
+		builder.AppendLine($"/// <inheritdoc />\r\npublic override void UpdateWith(object update, params string[] exclusions)\r\n{{\r\n\tswitch (update)\r\n\t{{\r\n\t\tcase {type.Name} options:\r\n\t\t{{\r\n\t\t\tUpdateWith(options, exclusions);\r\n\t\t\treturn;\r\n\t\t}}\r\n\t\tdefault:\r\n\t\t{{\r\n\t\t\tbase.UpdateWith(update, exclusions);\r\n\t\t\treturn;\r\n\t\t}}\r\n\t}}\r\n}}");
+
+		builder.ToString().CopyToClipboard();
+		Console.Write(builder.ToString());
 	}
+
+	#endregion
 }
