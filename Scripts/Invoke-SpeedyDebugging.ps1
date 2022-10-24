f38133f07009d9e32661daf78c5f6c523bbfd274
Bobby Cannon <bobby.cannon@live.com>
2022-10-24 12:23:48 -0400

    updating the Invoke-SpeedyDebugging script

--- Scripts/Invoke-SpeedyDebugging.ps1
+++ Scripts/Invoke-SpeedyDebugging.ps1
@@ -23,7 +23,7 @@ $scriptPath = $PSScriptRoot.Replace("\Scripts", "")
 
 # $scriptPath = "C:\Workspaces\EpicCoders\Speedy"
 # $scriptPath = "C:\Workspaces\GitHub\Speedy"
-# $Version = "8.4.8.0"
+# $Version = "11.0.0.0"
 # $VersionSuffix = "RC1"
 # $Framework = "netstandard2.0"
 
@@ -50,8 +50,6 @@ Write-Host "Version $version"
 Write-Host "Getting projects..."
 
 $files = Get-ChildItem $ProjectPath *.csproj -Recurse | Select-Object Fullname
-$path = $Framework.ToString()
-
 $projects = "Speedy",
 	"Speedy.Automation",
 	"Speedy.Application",
@@ -61,13 +59,12 @@ $projects = "Speedy",
 	"Speedy.EntityFramework",
 	"Speedy.ServiceHosting"
 	
-$projects
-
 $packageReferences = @()
 $packageReferences2 = @()
 $oldReferences = @()
 $directReferences = @()
 $directReferencesMarked = @()
+$platformReferences = @()
 
 foreach ($project in $projects)
 {
@@ -75,15 +72,24 @@ foreach ($project in $projects)
 	$packageReferences2 += "<PackageReference Include=`"$project`"><Version>$version</Version></PackageReference>"
 	$oldReferences += "<Reference Include=`"$project, Version=$versionFull, Culture=neutral, PublicKeyToken=8db7b042d9663bf8, processorArchitecture=MSIL`"><HintPath>..\packages\$project.$version\lib\$Framework\$project.dll</HintPath></Reference>"
 	$directReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\$Framework\$project.dll</HintPath></Reference>"
+		
+	# Specific frameworks
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\netstandard2.0\$project.dll</HintPath></Reference>"
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\netstandard2.1\$project.dll</HintPath></Reference>"
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\monoandroid10.0\$project.dll</HintPath></Reference>"
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\net5.0-windows\$project.dll</HintPath></Reference>"
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\net5.0-windows10.0.19041.0\$project.dll</HintPath></Reference>"
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\net6.0-windows\$project.dll</HintPath></Reference>"
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\net6.0-windows10.0.19041.0\$project.dll</HintPath></Reference>"
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\net6.0-android\$project.dll</HintPath></Reference>"
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\net6.0-ios\$project.dll</HintPath></Reference>"
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\net6.0-maccatalyst\$project.dll</HintPath></Reference>"
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\uap10.0.19041\$project.dll</HintPath></Reference>"
+	$platformReferences += "<Reference Include=`"$project`"><HintPath>$scriptPath\$project\bin\Debug\xamarin.ios10\$project.dll</HintPath></Reference>"
+	
 	# This allows us to roll back to the old package config direct references to local nuget files
 	$directReferencesMarked += "<Reference Include=`"$project`" PackageConfig=`"true`"><HintPath>$scriptPath\$project\bin\Debug\$Framework\$project.dll</HintPath></Reference>"
 }
-
-$packageReferences
-$packageReferences2
-$oldReferences
-$directReferences
-$directReferencesMarked
 
 foreach ($file in $files)
 {
@@ -116,6 +122,37 @@ foreach ($file in $files)
 			# Everything else used normal direct references
 			$data = $data.Replace($packageReferences[$i], $directReferences[$i])
 			$data = $data.Replace($packageReferences2[$i], $directReferences[$i])
+			
+			if ($data.Contains("<TargetPlatformIdentifier>UAP</TargetPlatformIdentifier>")) 
+			{
+				#Write-Host "UAP detected"
+				$data = $data.Replace("Speedy.Application.Xamarin\bin\Debug\netstandard2.0\Speedy.Application.Xamarin.dll", "Speedy.Application.Xamarin\bin\Debug\uap10.0.19041\Speedy.Application.Xamarin.dll")
+				$data = $data.Replace("Speedy.Application.Xamarin\bin\Debug\netstandard2.1\Speedy.Application.Xamarin.dll", "Speedy.Application.Xamarin\bin\Debug\uap10.0.19041\Speedy.Application.Xamarin.dll")
+			}
+			
+			if ($data.Contains("<TargetFramework>net5.0-windows</TargetFramework>") -or $data.Contains("<TargetFramework>net5.0-windows10.0.19041.0</TargetFramework>"))
+			{
+				#Write-Host ".NET 5 windows detected"
+				$data = $data.Replace("Speedy.Application.Wpf\bin\Debug\netstandard2.0\Speedy.Application.Wpf.dll", "Speedy.Application.Wpf\bin\Debug\net5.0-windows10.0.19041.0\Speedy.Application.Wpf.dll")
+				$data = $data.Replace("Speedy.Application.Wpf\bin\Debug\netstandard2.1\Speedy.Application.Wpf.dll", "Speedy.Application.Wpf\bin\Debug\net5.0-windows10.0.19041.0\Speedy.Application.Wpf.dll")
+				
+				$data = $data.Replace("Speedy.Application.Xamarin\bin\Debug\netstandard2.0\Speedy.Application.Xamarin.dll", "Speedy.Application.Xamarin\bin\Debug\net5.0-windows10.0.19041.0\Speedy.Application.Xamarin.dll")
+				$data = $data.Replace("Speedy.Application.Xamarin\bin\Debug\netstandard2.1\Speedy.Application.Xamarin.dll", "Speedy.Application.Xamarin\bin\Debug\net5.0-windows10.0.19041.0\Speedy.Application.Xamarin.dll")
+			}
+			
+			if ($data.Contains("<Reference Include=`"Mono.Android`" />"))
+			{
+				#Write-Host "Xamarin Android detected"
+				$data = $data.Replace("Speedy.Application.Xamarin\bin\Debug\netstandard2.0\Speedy.Application.Xamarin.dll", "Speedy.Application.Xamarin\bin\Debug\monoandroid10.0\Speedy.Application.Xamarin.dll")
+				$data = $data.Replace("Speedy.Application.Xamarin\bin\Debug\netstandard2.1\Speedy.Application.Xamarin.dll", "Speedy.Application.Xamarin\bin\Debug\monoandroid10.0\Speedy.Application.Xamarin.dll")
+			}
+			
+			if ($data.Contains("\Xamarin\iOS\Xamarin.iOS.CSharp.targets"))
+			{
+				#Write-Host "Xamarin iOS detected"
+				$data = $data.Replace("Speedy.Application.Xamarin\bin\Debug\netstandard2.0\Speedy.Application.Xamarin.dll", "Speedy.Application.Xamarin\bin\Debug\xamarin.ios10\Speedy.Application.Xamarin.dll")
+				$data = $data.Replace("Speedy.Application.Xamarin\bin\Debug\netstandard2.1\Speedy.Application.Xamarin.dll", "Speedy.Application.Xamarin\bin\Debug\xamarin.ios10\Speedy.Application.Xamarin.dll")
+			}
 		}	
 	}
 	
