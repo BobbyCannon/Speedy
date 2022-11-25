8db7bd7a47db0895d6f5243b678b57914ed19fd4
Bobby Cannon <bobby.cannon@live.com>
2022-11-25 15:36:35 -0500

    RC5 release for LocationComparer issues

--- Speedy.UnitTests/Data/Location/LocationComparerTests.cs
+++ Speedy.UnitTests/Data/Location/LocationComparerTests.cs
@@ -11,7 +11,45 @@ public class LocationComparerTests : SpeedyUnitTest
 	#region Methods
 
 	[TestMethod]
-	public void Test()
+	public void OnlyHorizontalShouldUpdate()
+	{
+		TestHelper.SetTime(new DateTime(2022, 11, 25, 01, 58, 12, DateTimeKind.Utc));
+
+		var comparer = new LocationComparer<SpeedyLocation, IHorizontalLocation, IVerticalLocation>();
+		var location = new SpeedyLocation();
+		var update = new SpeedyLocation
+		{
+			HorizontalLocation =
+			{
+				SourceName = "test"
+			},
+			VerticalLocation =
+			{
+				SourceName = "test"
+			}
+		};
+
+		AreEqual(false, comparer.ShouldUpdate(location, update));
+
+		update.HorizontalLocation.Latitude = 12.345;
+		update.HorizontalLocation.Longitude = 80.543;
+		AreEqual(false, comparer.ShouldUpdate(location, update));
+
+		update.HorizontalLocation.HasValue = true;
+		AreEqual(true, comparer.ShouldUpdate(location, update));
+		
+		update.HorizontalLocation.StatusTime = TimeService.UtcNow;
+		AreEqual(true, comparer.ShouldUpdate(location, update));
+		AreEqual(true, comparer.UpdateWith(ref location, update));
+
+		AreEqual(TimeService.UtcNow, location.HorizontalLocation.StatusTime);
+		AreEqual("test", location.HorizontalLocation.SourceName);
+		AreNotEqual(TimeService.UtcNow, location.VerticalLocation.StatusTime);
+		AreEqual(null, location.VerticalLocation.SourceName);
+	}
+	
+	[TestMethod]
+	public void OnlyVerticalShouldUpdate()
 	{
 		TestHelper.SetTime(new DateTime(2022, 11, 25, 01, 58, 12, DateTimeKind.Utc));
 
